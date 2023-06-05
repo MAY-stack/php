6341632b914b9223488fa4f9bc1f21023394efc9
MarisoL <120347424+MAY-stack@users.noreply.github.com>
2023-06-06 01:29:26 +0900

    Add files via upload

---
+++ www/UserRegister.php
@@ -0,0 +1,18 @@
+<?php
+    $con = mysqli_connect("localhost", "root", "12345", "root");
+
+    $userid = $_POST["userid"];
+    $userpassword = $_POST["userpassword"];
+    $usergender = $_POST["usergender"];
+    $usermajor = $_POST["usermajor"];
+    $useremail = $_POST["useremail"];
+    
+    $statement = mysqli_prepare($con, "INSERT INTO USER VALUES (?, ?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "sssss", $userid, $userpassword,$usergender,$usermajor,$useremail);
+    mysqli_execute($statement);
+
+    $response = array();
+    $response["success"] = true;
+
+    echo json_encode($response);
+?>
---
+++ www/UserValidate.php
@@ -0,0 +1,21 @@
+<?php
+    $con = mysqli_connect("localhost", "root", "12345", "root");
+
+    $userid = $_POST["userid"];
+   
+    $statement = mysqli_prepare($con, "SELECT * FROM USER WHERE userid = ?");
+    mysqli_stmt_bind_param($statement, "s", $userid);
+    mysqli_execute($statement);
+    mysqli_store_result($statement);
+    mysqli_stmt_bind_reult($statement);
+
+    $response = array();
+    $response["success"] = true;
+
+    while(mysqli_stmt_fetch($statement)){
+        $response["success"] = false;
+        $response["userid"] = $userid;
+    }
+    
+    echo json_encode($response);
+?>
