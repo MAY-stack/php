b64cb4a2ba2aed281fb605d5b05bd5f6ba75551b
MarisoL <120347424+MAY-stack@users.noreply.github.com>
2023-06-06 10:36:30 +0900

    Update UserValidate.php

--- www/UserValidate.php
+++ www/UserValidate.php
@@ -3,16 +3,16 @@
 
     $userid = $_POST["userid"];
    
-    $stmt = mysqli_prepare($con, "SELECT * FROM USER WHERE userid = ?");
-    mysqli_stmt_bind_param($stmt, "s", $userid);
-    mysqli_execute($stmt);
-    mysqli_store_result($stmt);
-    mysqli_stmt_bind_reult($stmt);
+    $statement = mysqli_prepare($con, "SELECT * FROM USER WHERE userid = ?");
+    mysqli_stmt_bind_param($statement, "s", $userid);
+    mysqli_execute($statement);
+    mysqli_stored_result($statement);
+    mysqli_stmt_bind_reult($statement);
 
     $response = array();
     $response["success"] = true;
 
-    while(mysqli_stmt_fetch($stmt)){
+    while(mysqli_stmt_fetch($statement)){
         $response["success"] = false;
         $response["userid"] = $userid;
     }
